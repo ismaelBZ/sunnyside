897f19af4ece4f337c16399e4a5f48bddad96dcc
Ismael <ismaelbetzoletti@gmail.com>
2024-02-07 07:32:04 -0300

    Added Menu functionality

--- hamburguer.js
+++ hamburguer.js
@@ -1,23 +1,56 @@
+const triangle = document.querySelector(".triangle");
 const hamburguer = document.getElementById("hamburguer");
-const menuMobile = document.querySelector(".menu");
+const menu = document.querySelector(".menu");
+var isMobileMenuOpen = null;
 
+/* Hamburguer Function */
 window.addEventListener("click", (e) => {
-  const isDisplayingMenu = window.getComputedStyle(menuMobile).display == "block";
+  const isDisplayingMenu = window.getComputedStyle(menu).display == "block";
   if (e.target == hamburguer) {
     if (isDisplayingMenu) {
-      menuMobile.style.display = "none";
+      menu.style.display = "none";
+      triangle.style.display = "none";
+      isMobileMenuOpen = false;
     } else if (!isDisplayingMenu) {
-      menuMobile.style.display = "block";
+      menu.style.display = "block";
+      triangle.style.display = "block";
+      isMobileMenuOpen = true;
     }
   }
 
-  if (e.target !== menuMobile && isDisplayingMenu) {
-    menuMobile.style.display = "none";
+  if (e.target !== menu && isDisplayingMenu) {
+    menu.style.display = "none";
+    triangle.style.display = "none";
+    isMobileMenuOpen = false;
   }
 });
 
-// window.addEventListener("resize", () => {
-//   if (window.matchMedia("(min-width: 550px)").matches) {
-//     menuMobile.style.display = "none";
-//   }
-// });
+/* Show Triangle */
+window.addEventListener("resize", () => {
+  const isDisplayingMenu = window.getComputedStyle(menu).display == "block";
+
+  if (window.matchMedia("(min-width: 550px)").matches) {
+    triangle.style.display = "none";
+  } else if (isMobileMenuOpen && window.matchMedia("(max-width: 550px").matches) {
+    if (isDisplayingMenu) {
+      triangle.style.display = "block";
+    }
+  }
+});
+
+/* Responsive Menu */
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(max-width: 550px").matches) {
+    if (isMobileMenuOpen) {
+      menu.style.display = "block";
+    } else if (!isMobileMenuOpen) {
+      menu.style.display = "none";
+    }
+  }
+  if (window.matchMedia("(min-width: 550px").matches) {
+    menu.style.display = "block";
+    isMobileMenuOpen = false;
+  }
+});
+
+
